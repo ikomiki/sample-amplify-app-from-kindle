346e493c209f78a34cd11b60813fb1528aa357e2
matsuda-t <matsuda-t@technonet.co.jp>
2023-06-17 12:54:29 +0900

    データ(pull)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "Sampleappfromkindle": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "Sampleappfromkindle": {
       "AppClientID": "string",
